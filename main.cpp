f60742f089a0541ec9c64bd569aca341e6062108
radekeolumogba <147517202+radekeolumogba@users.noreply.github.com>
2025-07-13 19:00:41 +0100

    Blackjack Assignmt to display OOP Data structures
    For sample of skeleton based assignment, individually solved - code written by me but some comments from professor (guided).
    To display efficient use of data structures, classes designed, iterators, memory savings etc...

--- main.cpp
+++ main.cpp
@@ -1,19 +1,59 @@
-#include "mbed.h" // using mbed LPC1768
+//  Blackjack game code.  Dr. Klein 6/20/2016
+#include "Blackjack.h"
 #include <iostream>
-#include "PingPong_Header.h"
 
 using namespace std;
 
-int main() {
-	while (true)
-	{
-        int n = 1; // Test left movement
-        PingPong Game; // Start the game
+//  Function to take player's bet.
+double takeBet(const double & playerMoney)
+{
+  double bet; 
 
-        Game.Game_StateMachine(n); // start left movement
+  do 
+  {
+  cout << "Enter bet in dollars, 0 to quit: ";
+  cin >> bet;
+  if (bet > playerMoney)
+    cout << "You don't have that much money!  Enter a lower bet." << endl;
+  } while (bet > playerMoney);
 
-        n = 4; // Test right movement
-        Game.Game_StateMachine(n); // start right movement
-		
-	}
+  return bet;
+}
+
+
+//  main function
+int main()
+{
+//  Seed the random number generator with current time
+  std::srand( static_cast<unsigned int>(time(0)));
+
+//  Player starts with 300 dollars
+  double playerMoney = 300.0, playerBet;
+//  integer to hold the round number
+  int roundNum = 1;
+
+  cout << "Player starts with " << playerMoney << " dollars" << endl;
+
+  cout << "Round " << roundNum << endl;
+//  Take bet
+  playerBet = takeBet(playerMoney); 
+
+
+//  Keep playing Blackjack until the player bets 0 dollars, or they are broke.
+  while ((playerBet > 0.0) && (playerMoney > 0.0))
+  { 
+//   Instantiate Blackjack object, which initializes round. 
+    Blackjack myBlackjack(playerMoney); 
+//   Play the round of Blackjack.  Member function 'play' should return the updated
+//     playerMoney.
+    playerMoney = myBlackjack.play(playerBet);
+
+//   Take a new bet unless the player is broke.
+    if (playerMoney > 0.0)
+    {
+      cout << endl << endl << "Round " << ++roundNum << endl;
+      playerBet = takeBet(playerMoney); 
+    }
+  }
+
 }
